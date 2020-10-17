245e31c3c7d33d6bd506a211bc8a01b986ca1311
1DWalker <alex.j.sheng.yang@gmail.com>
2020-10-17 16:55:14 -0400

    [29] Added image select base file

---
+++ src/components/ImageSelect.js
@@ -0,0 +1,50 @@
+import React, { Component, useState } from 'react';
+import { BrowserRouter as Router, Route, useHistory} from "react-router-dom";
+import logo from "../res/images/76ers.png"
+
+import "../styling/ImageSelect.css"
+
+function Tile(props) {
+  console.log(props.path === "../res/images/76ers.png");
+  const key = props.key;
+  const path = props.path;
+  // const img = require("../res/images/76ers.png");
+  // const img = require(props.path);
+
+  const [selected, setSelected] = useState(false);
+
+  const name = "76ers.png";
+
+  return (
+  <td className="imageselect-tile" onClick={() => console.log("clicked") }>
+    <img className="imageselect-image" src={require("../res/images/${name}")} ></img>
+  </td> 
+  )
+}
+
+export default function ImageSelect(props) {
+  return (
+    <div className="imageselect-container">
+      <table className="imageselect-table">
+        <tbody>
+          <tr>
+            {/* <td className="imageselect-tile" onClick={() => console.log("clicked") }>
+              <img className="imageselect-image" src={logo}></img>
+            </td> */}
+            <Tile key="" path="../res/images/76ers.png" />
+
+            <td className="imageselect-tile">
+              <img className="imageselect-image" src={logo}></img>
+            </td>
+            <td className="imageselect-tile">Jill</td>
+          </tr>
+          <tr>
+            <td className="imageselect-tile">Jill</td>
+            <td className="imageselect-tile">Jill</td>
+            <td className="imageselect-tile">Jill</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  )
+}
