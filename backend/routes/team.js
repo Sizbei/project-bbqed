cef5c835014dbb758303c7f57e2431953697eb76
Andy Phy Lim <Andyphyl00@gmail.com>
2020-10-17 01:22:45 -0400

    [29] Update to teams route

--- backend/routes/team.js
+++ backend/routes/team.js
@@ -3,10 +3,13 @@ let Team = require('../models/team');
 
 //get method
 router.route('/').get((req, res) => {
-    const name = req.body.name;
-    Team.findOne({name: name})
-        .then((team) => res.json(team.image))
+    Team.find()
+        .then((team) => res.json(team))
         .catch(err => res.status(400).json('Error: ' + err));
+    // const name = req.body.name;
+    // Team.findOne({name: name})
+    //     .then((team) => res.json(team.image))
+    //     .catch(err => res.status(400).json('Error: ' + err));
 });
 
 module.exports = router;
