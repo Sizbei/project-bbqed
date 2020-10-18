dce03ae2137130c47200f30a617344207b86a598
1DWalker <alex.j.sheng.yang@gmail.com>
2020-10-18 18:32:27 -0400

    [10] Created route to settings/profile

--- src/Homepage.js
+++ src/Homepage.js
@@ -9,6 +9,7 @@ import Analysis from "./components/Analysis";
 import PicksAndPredictions from "./components/PicksAndPredictions";
 import Profile from "./components/Profile";
 import Registration from "./components/Registration"
+import EditProfile from "./components/EditProfile"
 
 function App() {
   return (
@@ -21,6 +22,7 @@ function App() {
       <Route path="/PicksAndPredictions" component={PicksAndPredictions} />
       <Route path="/Profile" component={Profile} />
       <Route path="/Registration" component={Registration} />
+      <Route path="/Settings/Profile" component={EditProfile} />
       </div>
     </Router>
   );
