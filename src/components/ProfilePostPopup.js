da3dccd6828940f0822c1214565b9852cb2c2124
Cherie <c.kong@mail.utoronto.ca>
2020-10-20 00:33:44 -0400

    [36] changed naming scheme of css

--- src/components/ProfilePostPopup.js
+++ src/components/ProfilePostPopup.js
@@ -3,12 +3,33 @@ import React from 'react';
 import '../styling/ProfilePopup.css'  
 
 class Popup extends React.Component {  
+  constructor(props){
+    super(props);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(event) {
+    const data = {
+      user: 'something'
+
+    }
+    alert('You have created post!')
+  }
   render() {  
   return (  
-  <div className='popup'>  
-    <div className='popup-inner'>  
-    <h1>{this.props.text}</h1>  
-    <button onClick={this.props.closePopup}>close me</button>  
+  <div className='profile-popup'>  
+    <div className='profile-popup-popup-content'>  
+      <div>
+        <button className="profile-popup-close-button" onClick={this.props.closePopup}> X </button>  
+      </div>
+      
+      <div className="profile-popup-content">
+        <h1> Create a Post </h1>
+        <inut className="profile-popup-input" name="post-content"/>
+        <button className="profile-popup-submit-button" onClick={this.handleSubmit}> Post </button>
+      </div>
+      
+
     </div>  
   </div>  
   );  
