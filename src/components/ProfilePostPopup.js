90606c2e274dcfde946d3f968beb314b52995480
Cherie <c.kong@mail.utoronto.ca>
2020-10-20 00:16:24 -0400

    Revert "[36] Created basic ui for post submission"
    This reverts commit 6184cb71a8cbef7d092ab5cc96222c254788175d.

--- src/components/ProfilePostPopup.js
+++ src/components/ProfilePostPopup.js
@@ -3,32 +3,12 @@ import React from 'react';
 import '../styling/ProfilePopup.css'  
 
 class Popup extends React.Component {  
-  constructor(props){
-    super(props);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  handleSubmit(event) {
-    const data = {
-      user: 'something'
-    }
-    alert('You have created post!')
-  }
   render() {  
   return (  
   <div className='popup'>  
-    <div className='popup-content'>  
-      <div>
-        <button className="close-button" onClick={this.props.closePopup}> X </button>  
-      </div>
-      
-      <div className="post-content">
-        <h1> Create a Post </h1>
-        <input className="input" name="post-content"/>
-        <button className="submit-button" onClick={this.handleSubmit}> Post </button>
-      </div>
-      
-
+    <div className='popup-inner'>  
+    <h1>{this.props.text}</h1>  
+    <button onClick={this.props.closePopup}>close me</button>  
     </div>  
   </div>  
   );  
