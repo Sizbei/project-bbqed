3650ed46389d0482b3ab7abd40c48d64e3eea969
Cherie <c.kong@mail.utoronto.ca>
2020-10-20 17:16:57 -0400

    [34] error checking for post popup

---
+++ src/components/RadarList.js
@@ -0,0 +1,14 @@
+/*
+import React, { Component, useEffect, useState } from 'react';
+import { BrowserRouter as Router, Route, useHistory} from "react-router-dom";
+import { useForm } from "react-hook-form";
+import axios from 'axios';
+
+export default function RadarList(() => {
+
+  useEffect(() => {
+
+  }
+  )
+}
+*/
