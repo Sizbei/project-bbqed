5f1f8f340a59087a63a7aa1658de9acbaed19d00
1DWalker <alex.j.sheng.yang@gmail.com>
2020-10-27 19:45:03 -0400

    [44] Added checkmarks and crossmarks

--- src/components/TriviaSidebar.js
+++ src/components/TriviaSidebar.js
@@ -5,7 +5,7 @@ import '../styling/TriviaSidebar.css';
 import icon from '../res/images/puzzle-piece.png'
 
 export default function TriviaSidebar(props) {
-  const [nav, setNav] = useState(true);  
+  const [nav, setNav] = useState(false);  
 
   const handleClickOnline = e => {
     e.stopPropagation();
@@ -22,6 +22,16 @@ export default function TriviaSidebar(props) {
   const handleClickSolo = e => {
     e.stopPropagation();
   }
+
+  const checkmark = (
+    <div className="TSBG-list-item-checkmark-div">
+      <div className="TSBG-list-item-checkmark"></div>
+    </div>
+  )
+
+  const crossmark = (
+    <div className="TSBG-list-item-crossmark">&times;</div>
+  )
 
   if (nav) {
     return (
@@ -53,21 +63,46 @@ export default function TriviaSidebar(props) {
   } else {
     return (
       <div className="TSB-div">
-        <div className="TSBG-header">
+        <div className="TSB-header">
+          <span className="TSB-header-text">Trivia!</span>
+          <div className="TSB-header-icon"></div>
         </div>
+
+        {/* <div className="TSBG-header">
+        </div> */}
+
         <div className="TSBG-list">
           <div className="TSBG-list-item TSBG-list-item-e">
-            <span className="TSBG-list-item-number"> 1. &emsp; </span>
-            <span className="TSBG-list-item-text"> Here lies the first few words of the question...</span>
+            <span className="TSBG-list-item-baseline">
+              <span className="TSBG-list-item-number"> &nbsp; 1. &nbsp; </span>
+              <span className="TSBG-list-item-text"> Here lies the first few words of the question...</span>
+            </span>
+            <div className="TSBG-list-item-checks">
+              {checkmark}
+              {crossmark}
+            </div>
           </div>
           <div className="TSBG-list-item TSBG-list-item-o">
-            <span className="TSBG-list-item-number"> 2.</span>
+            <span className="TSBG-list-item-number"> &nbsp; 2. &nbsp;</span>
+            <div className="TSBG-list-item-checks">
+              {crossmark}
+              {checkmark}
+            </div>
           </div>
           <div className="TSBG-list-item TSBG-list-item-e">
-            <span className="TSBG-list-item-number"> 3.</span>
+            <span className="TSBG-list-item-number"> &nbsp; 3. &nbsp;</span>
+            <div className="TSBG-list-item-checks">
+              {checkmark}
+              {checkmark}
+            </div>
           </div>
           <div className="TSBG-list-item TSBG-list-item-o">
-            <span className="TSBG-list-item-number"> 10.</span>
+            <span className="TSBG-list-item-number"> 10. &nbsp; </span>
+            <span className="TSBG-list-item-text"> Here lies the few words of the question...</span>
+            <div className="TSBG-list-item-checks">
+              {crossmark}
+              {crossmark}
+            </div>
           </div>
         </div>
       </div>
