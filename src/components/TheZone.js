f72c31abf7de5ae6e17c44f666c5c4b68606c6f5
Raymond Chen <raymondchen2009@live.cn>
2020-10-26 22:08:48 -0400

    [37] Removed errors from TheZone.js

--- src/components/TheZone.js
+++ src/components/TheZone.js
@@ -1,97 +1,55 @@
 import React, {Component} from 'react';
 import axios from 'axios';
+import '../styling/TheZone.css';
+import PostPopup from './ProfilePostPopup';
 import Header from './Header';
+import { AuthContext } from '../Context/AuthContext';
 
 export default class TheZone extends Component {
+    //example of using authContext in class
+    //define contextType in the class using AuthContext
+    //so that we are able to access it through this.context
+    static contextType = AuthContext;
+
     constructor(props) {
         super(props);
 
-        //Binds to methods that listen for events
-        this.onChangeUsername = this.onChangeUsername.bind(this);
-        this.onChangePassword = this.onChangePassword.bind(this);
-        this.onLogin = this.onLogin.bind(this);
+    }
 
-        //variables
-        this.state = {
-            username: '',
-            password: ''
-        }
+    //Listens for an event and creates content for a post
+    onCreatePost(e) {
+        
+        document.getElementById('post').style.borderColor = "transparent";
     }
 
     //Listens for an event and sets the username state
-    onChangeUsername(e) {
-        this.setState({
-            username: e.target.value
-        })
-        document.getElementById('username').style.borderColor = "transparent";
-        document.getElementById('password').style.borderColor = "transparent";
+    onComment(e) {
+        document.getElementById('comment').style.borderColor = "transparent";
     }
 
-    //Listens for an event and sets the username state
-    onChangePassword(e) {
-        this.setState({
-            password: e.target.value
-        })
-        document.getElementById('username').style.borderColor = "transparent";
-        document.getElementById('password').style.borderColor = "transparent";
-    }
+    //onReportPost(e){}
 
-    onLogin(e) {
-        //prevents default html form submit from taking place
-        e.preventDefault();
-        //Creates a body for a db call with the current variables
-        const userBody = {
-            params: {
-                username: this.state.username,
-                password: this.state.password
-            }
-        }
+    //onDeletePost(e) {}
 
-        axios.get('http://localhost:5000/login', userBody)
-            .then(res => {
-                if (res.data.authenticated) {
-                    window.location.href = '/TheZone';
-                }
-                else {
-                    document.getElementById('username').style.borderColor = "red";
-                    document.getElementById('password').style.borderColor = "red";
-                }
-            });
-        console.log(userBody);
-    }
 
     render() {
         return (
             <div className="page">
                 <Header />
                 <div className="login">
-                    <img src={logo} className="logo" />
-                    <label className="slogan">Start Building Your ACS Score</label>
-                    <div className="identifier">
-                        <label className="text">Username or Email</label>
+                   
+                    <div className="post">
+                        <label className="text">Create Post</label>
                         <input className="input"
-                            id="username"
+                            id="post"
                             type="string"
-                            value={this.state.username}
-                            onChange={this.onChangeUsername} />
+                            
+                             />
                     </div>
-                    <div className="identifier">
-                        <label className="text" >Password</label>
-                        <input className="input"
-                            id="password"
-                            type="password"
-                            value={this.state.password}
-                            onChange={this.onChangePassword} />
-                        <a href="" className="passwordlink">Forgot Password?</a>
-                    </div>
-                    <button className="loginBtn" onClick={this.onLogin}>Log In</button>
+                  
+                    <button className="createPostBtn" onClick={this.onLogin}>Create Post</button>
                     <br></br>
-                    <div className="signup">
-                        <label className="boldtext">Not a Member?</label>
-                    </div>
-                    <div className="signup">
-                        <Signup />
-                    </div>
+                   
                 </div>
             </div>
         );
