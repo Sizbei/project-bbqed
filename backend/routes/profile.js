d6c67212d3f4e358661cf2c9406d3f4f8fee8300
Deon Li <34903484+lideon@users.noreply.github.com>
2020-10-27 22:34:58 -0400

    [55] Fixed bug where Radar List sent previous user request

--- backend/routes/profile.js
+++ backend/routes/profile.js
@@ -8,10 +8,6 @@ let Teams = require('../models/team');
 let Acs = require('../models/acs');
 const { isAssertionExpression } = require('typescript');
 
-let tempAcs = 0;
-var tempPic = "";
-var radarList = [];
-
 //get method
 router.route('/:username').get(passport.authenticate('jwt', {session : false}),(req, res) => {
   Profile.findOne({username: req.params.username})
@@ -28,6 +24,9 @@ router.route('/:username/teams').get(passport.authenticate('jwt', {session : fal
 router.route('/:username/radarlist').get(async (req,res) => {
   Profile.findOne({username: req.params.username})
   .then(async (user) => {
+    let tempAcs = 0;
+    var tempPic = "";
+    var radarList = [];
     for (var i = 0; i < user.radarList.length; i++) {
       tempAcs = await Acs.findOne({username:user.radarList[i]}).then(acs => {return acs.acsTotal[0].total});
       tempPic = await Profile.findOne({username: user.radarList[i]}).then(profile => {return profile.image});
@@ -35,6 +34,24 @@ router.route('/:username/radarlist').get(async (req,res) => {
     }
     res.json({radarList: radarList})})
   .catch(err => res.status(400).json('Error: ' + err));
+})
+
+router.route('/:username/:viewing/checkRadar').get(async (req,res) => {
+  await Profile.findOne({username:req.params.username}).then(async (user) => {
+    var following = false;
+    for (var i = 0; i < user.radarList.length; i ++) {
+      if (user.radarList[i] === req.params.viewing) {
+        following = true;
+      }
+    }
+    res.json({"following":following})
+  }).catch(err => res.status(400).json('Error ' + err));
+})
+
+router.route('/:username/addRadar').put(async (req, res) => {
+  await Profile.updateOne({username: req.body.username}, {$push: {radarList:[req.body.viewing]}}).then(
+    res.json("Added friend")
+  ).catch(err => res.status(400).json('Error ' + err));
 })
 
 module.exports = router;
