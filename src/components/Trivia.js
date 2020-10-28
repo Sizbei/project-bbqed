26e0a13a08b795e41dc847d768665cbf17d04035
Andy Phy Lim <Andyphyl00@gmail.com>
2020-10-28 17:00:12 -0400

    [44, 75] Add InGameTrivia UI

--- src/components/Trivia.js
+++ src/components/Trivia.js
@@ -1,12 +1,13 @@
 import React, {Component} from 'react';
 import axios from 'axios';
 import Header from './Header';
+import InGameTrivia from './InGameTrivia';
 
 export default class Trivia extends Component {
     render() {
         return(
         <div>
-          <p>You're on Trivia.</p>
+          <InGameTrivia/>
         </div>
         );
     }
