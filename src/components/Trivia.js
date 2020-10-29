1fb19800834582efc63126be68ac2c8d1b9659e1
1DWalker <alex.j.sheng.yang@gmail.com>
2020-10-29 05:32:44 -0400

    [4] Integrated the transition from sidebar select to singleplayer game

--- src/components/Trivia.js
+++ src/components/Trivia.js
@@ -7,19 +7,7 @@ import AuthService from '../Services/AuthService';
 import TriviaSidebar from './TriviaSidebar';
 
 export default function Trivia(props) {
-  const [state, setState] = useState({});
-  // const initialTriviaProps = {
-  //   mode: mode,
-  //   ourUsername: "user1",
-  //   enemyUsername: "user3",
-  //   gameOver: false,
-
-  //   questionNumber: 5,
-  //   currentQuestion: "If A eats B and B eats C, then can A eat C?",
-  //   options: ["happy feet", "wombo combo", "**incoherent screaming", "..."],
-  // }
-
-  const [triviaProps, setTriviaProps] = useState(null);
+  const [state, setState] = useState({ mode:"nav" });
   const authContext = useContext(AuthContext);
 
   const handleModeSelect = mode => {
@@ -52,41 +40,37 @@ export default function Trivia(props) {
   const handleOptionSelect = option => {
     console.log(option);
 
-    const next = {
-      instance: state.instance,
-      question: state.triviaProps.currentQuestion,
-      answer: state.triviaProps.options[option]
+    if (state.mode === "singlePlayer") {
+      const next = {
+        instance: state.instance,
+        question: state.triviaProps.currentQuestion,
+        answer: state.triviaProps.options[option]
+      }
+  
+      console.log("SENT", next);
+  
+      axios.put("/trivia/solo/next", next).then (nextData => {
+        console.log("TRIVIA NEXT", nextData.data);
+        const newState = {...state};
+        newState.triviaProps = nextData.data;
+        setState(newState);
+      }).catch(e => {
+        console.log("some error", e);
+      })
     }
-
-    console.log("SENT", next);
-
-    axios.put("/trivia/solo/next", next).then (nextData => {
-      console.log("TRIVIA NEXT", nextData.data);
-      const newState = {...state};
-      newState.triviaProps = nextData.data;
-      setState(newState);
-    }).catch(e => {
-      console.log("some error", e);
-    })
   }
-
-  // const triviaPage = "triviaProps" in state ? <InGameTrivia {...state.triviaProps} 
-  // handleOptionSelect={handleOptionSelect} /> : null;
 
   const [triviaPage, setTriviaPage] = useState(null);
   
   useEffect(() => {
-    // console.log("NOTHING IS HAPPENING", JSON.stringify(state));
-    let nextTriviaPage = "triviaProps" in state ? <InGameTrivia {...state.triviaProps} 
-      handleOptionSelect={handleOptionSelect} /> : null;
+    let nextTriviaPage = <InGameTrivia {...state.triviaProps} 
+      mode={state.mode} handleModeSelect={handleModeSelect} handleOptionSelect={handleOptionSelect} />;
     setTriviaPage(nextTriviaPage);
   }, [state]);
 
   return(
     <div>
       {triviaPage}
-      {/* <TriviaSidebar /> */}
-      <button onClick={() => handleModeSelect("singlePlayer")}> Single Player</button>
     </div>
   );
 }
