c9df3bf786b3d4f015197d201c92ca4b114d72ac
Deon Li <34903484+lideon@users.noreply.github.com>
2020-10-29 17:09:07 -0400

    Issue 8 Completed Profiles for other players, functionality & ACS functionality. (#19)
    * [64] Created ACS routes, updated ACS model, signup now creates ACS model
    
    * [64] Updated Acs model for spelling, Finished acs route for profile
    
    * [64] ACS Score breakdown now sends numbers instead of Strings
    
    * [64] Edited profile routes and privated them, also now sends total ACS score
    
    * [64] Edited profile routes and privated them, also now sends total ACS score
    
    * [64] Edited DB scripts
    
    * [64] Updated Scripts to handle user4
    
    * [53] Modified css for profile and radar list
    
    Co-authored-by: Cherie <c.kong@mail.utoronto.ca>

--- backend/routes/profile.js
+++ backend/routes/profile.js
@@ -21,14 +21,14 @@ router.route('/:username/teams').get(passport.authenticate('jwt', {session : fal
     .catch(err => res.status(400).json('Error: ' + err));
 });
 
-router.route('/:username/radarlist').get(async (req,res) => {
+router.route('/:username/radarlist').get(passport.authenticate('jwt', {session : false}), async (req,res) => {
   Profile.findOne({username: req.params.username})
   .then(async (user) => {
     let tempAcs = 0;
     var tempPic = "";
     var radarList = [];
     for (var i = 0; i < user.radarList.length; i++) {
-      tempAcs = await Acs.findOne({username:user.radarList[i]}).then(acs => {return acs.acsTotal[0].total});
+      tempAcs = await Acs.findOne({username:user.radarList[i]}).then(acs => {return acs.acsTotal.total});
       tempPic = await Profile.findOne({username: user.radarList[i]}).then(profile => {return profile.image});
         radarList[i] = {username: user.radarList[i], acs:tempAcs, profilePic:tempPic}
     }
@@ -36,7 +36,7 @@ router.route('/:username/radarlist').get(async (req,res) => {
   .catch(err => res.status(400).json('Error: ' + err));
 })
 
-router.route('/:username/:viewing/checkRadar').get(async (req,res) => {
+router.route('/:username/:viewing/checkRadar').get(passport.authenticate('jwt', {session : false}), async (req,res) => {
   await Profile.findOne({username:req.params.username}).then(async (user) => {
     var following = false;
     for (var i = 0; i < user.radarList.length; i ++) {
@@ -48,17 +48,54 @@ router.route('/:username/:viewing/checkRadar').get(async (req,res) => {
   }).catch(err => res.status(400).json('Error ' + err));
 })
 
-router.route('/:username/addRadar').put(async (req, res) => {
+router.route('/:username/addRadar').put(passport.authenticate('jwt', {session : false}), async (req, res) => {
   await Profile.updateOne({username: req.body.username}, {$push: {radarList:[req.body.viewing]}}).then(
     res.json("Added friend")
   ).catch(err => res.status(400).json('Error ' + err));
 })
 
-router.route('/:username/removeRadar').delete(async(req,res) => { 
+router.route('/:username/removeRadar').delete(passport.authenticate('jwt', {session : false}), async(req,res) => { 
   console.log(req.body)
   await Profile.updateOne({username: req.body.username}, {$pullAll: {radarList:[req.body.viewing]}}).then(
     res.json("Removed friend")
   ).catch(err => res.status(400).json('Error ' + err));
 });
+
+router.route('/:username/acs').get(passport.authenticate('jwt', {session : false}), async(req, res) => {
+  await Acs.findOne({username:req.params.username}).then(
+    (user) => {
+      var acsChart = []
+      acsChart[0] = {title: "Trivia & Games", value: user.acsTotal.triviaGames/user.acsTotal.total}
+      acsChart[1] = {title: "Analysis & Debate", value: user.acsTotal.analysisDebate/user.acsTotal.total}
+      acsChart[2] = {title: "Picks & Prediction", value: user.acsTotal.picksPrediction/user.acsTotal.total}
+      acsChart[3] = {title: "Participation & History", value: user.acsTotal.participationHistory/user.acsTotal.total}
+      var currentTime = Date.now()
+      var toProcess = user.acsHistory.slice(Math.max(user.acsHistory.length - 5, 0))
+      var newEditedAcsHistory = []
+      for (var i = 0; i < toProcess.length; i++) {
+        newEditedAcsHistory[i] = {category: toProcess[i].category, points: toProcess[i].points, date: dateDifference(currentTime, toProcess[i].date)}
+      }
+      var acs = {acsChart: acsChart, acsHistory: newEditedAcsHistory.reverse(), acsTotal:user.acsTotal.total}
+      res.json(acs)
+    }
+  ).catch(err => res.status(400).json('Error ' + err));
+})
+
+function dateDifference(now,then) {
+  var timeElapsedinSeconds = (now - then)/1000;
+  var timeElapsedinMinutes =  timeElapsedinSeconds/60;
+  if (timeElapsedinMinutes < 1) {
+    return 'A few seconds Ago';
+  }
+  var timeElapsedinHours = timeElapsedinMinutes/60
+  if (timeElapsedinHours < 1) {
+    return Math.floor(timeElapsedinMinutes).toString() + " Minutes Ago"
+  }
+  var timeElapsedinDays = timeElapsedinHours/24
+  if (timeElapsedinDays < 1) {
+    return Math.floor(timeElapsedinHours).toString() + " Hours Ago"
+  } 
+  return Math.floor(timeElapsedinDays).toString() + " Days Ago"
+}
 
 module.exports = router;
