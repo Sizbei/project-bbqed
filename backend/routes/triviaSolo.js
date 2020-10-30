7c57bc04280faa559fa0028712a6f095e720cb05
axelan <axelan@users.noreply.github.com>
2020-10-30 02:11:32 -0400

    [68] Added time checking in trivia router

--- backend/routes/triviaSolo.js
+++ backend/routes/triviaSolo.js
@@ -31,70 +31,86 @@ router.route('/next').put((req, res) => {
   instance.findOne({_id: req.body.instance})
   .then(game => {
 
-    console.log(game);
+    // console.log(game);
 
-    if(game.questionIds.length == 10){
-      acs.findOne({username: game.username})
-        .then(userACS => 
-        {
-          console.log("username found.");
-          const entry = {
-            category: "Picks&Predictions",
-            points: game.points,
-            date: new Date()
-          }
-          userACS.acsHistory.push(entry);
-          userACS.acsTotal.total += game.points;
-          userACS.acsTotal.picksPrediciton += game.points;
-          userACS.save()
-            .then(() => res.json({acs: userACS.acsTotal.total, points: game.points}))
-            .catch(err => res.status(400).json('Error: ' + err));
-
-      }).catch(err => res.status(401).json('Error: ' + err));
-
-    } else {
-      console.log("hello");
-
-      const sendRandom = (previous) => {
-        trivia.aggregate([
-          { $match: { _id: { $nin: game.questionIds } } },
-          { $sample: { size: 1 } } 
-        ]).then(q => {
-          const question = q[0];
-          console.log("question", question);
-            game.questionIds.push(question);
-            console.log("bad");
-            game.save()
-              .then(() => {
-                console.log("good");
-                console.log({ currentQuestion:question.question, 
-                  options:question.options, previous: previous, questionCount: game.questionIds.length });
-              return res.json({ currentQuestion:question.question, 
-                options:question.options, previous: previous, questionCount: game.questionIds.length })
-              })
-              .catch(err => res.status(403).json('Error: ' + err));
-          }).catch(err => res.status(404).json('Error: ' + err));
-      }
-
-      if(game.questionIds.length > 0){
-        console.log("hellow");
-        console.log("not running");
-        trivia.findOne({question: req.body.question})
-          .then(response => {
-            console.log("step 2");
-            if(response.answer === req.body.answer){
-              console.log("step 3");
-              game.points += 1;
-              game.save().then(() => sendRandom("correct"));
-            } else {
-              game.points -= 1;
-              game.save().then(() => sendRandom("wrong"));
-            }
-          }).catch(err => res.status(402).json('Error: ' + err));
-      } else {
-        sendRandom("none");
-      }
+    const sendRandom = (previous) => {
+      trivia.aggregate([
+        { $match: { _id: { $nin: game.questionIds } } },
+        { $sample: { size: 1 } } 
+      ]).then(q => {
+        const question = q[0];
+        game.questionIds.push(question);
+        game.times.push(new Date())
+        game.save()
+          .then(() => {
+            res.json({ currentQuestion:question.question, 
+            options:question.options, previous: previous, questionCount: game.questionIds.length })
+          })
+          .catch(err => res.status(403).json('Error: ' + err));
+      }).catch(err => res.status(404).json('Error: ' + err));
     }
+
+    const sendDone = (previous) => {
+      acs.findOne({username: game.user})
+      .then(userACS => 
+      {
+        const entry = {
+          category: "Picks & Predictions",
+          points: game.points,
+          date: new Date()
+        }
+
+        userACS.acsHistory.push(entry);
+        userACS.acsTotal.total += game.points;
+        userACS.acsTotal.picksPrediciton += game.points;
+        
+        game.inProgress = false;
+        game.save().catch(err => res.status(400).json('Error: ' + err));
+
+        userACS.save()
+          .then(() => res.json({acs: userACS.acsTotal.total, points: game.points, previous: previous}))
+          .catch(err => res.status(400).json('Error: ' + err));
+
+    }).catch(err => res.status(401).json('Error: ' + err));
+    }
+
+    const checkTime = (time) => {
+      const curTime = new Date();
+      if(curTime - time < 10000){
+        return true;
+      }
+      return false;
+
+    }
+
+    if(game.questionIds.length > 0){
+      trivia.findOne({question: req.body.question})
+        .then(response => {
+          if(response.answer === req.body.answer && checkTime(game.times[game.questionIds.length - 1])){
+            game.points += 1;
+            game.save().then(() => {
+              if (game.questionIds.length < 10) {
+                sendRandom("correct")
+              } else {
+                sendDone("correct")
+              }
+            });
+          } else {
+            game.points -= 1;
+            game.save().then(() => {
+              if (game.questionIds.length < 10) {
+                sendRandom("wrong")
+              } else {
+                sendDone("wrong")
+              }
+            });
+          }
+        }).catch(err => res.status(402).json('Error: ' + err));
+    } else {
+      sendRandom("none");
+    }
+      
+    
   }).catch(err => res.status(405).json('Error: ' + err));
 });
 
