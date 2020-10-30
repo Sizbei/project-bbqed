cd08d305c52ce5c7e99df355a3bf9e27e207a120
axelan <axelan@users.noreply.github.com>
2020-10-29 23:12:10 -0400

    [68] Updated route for solo trivia

--- backend/routes/triviaSolo.js
+++ backend/routes/triviaSolo.js
@@ -13,11 +13,16 @@ router.route('/create').post((req, res) => {
     user: user
   })
 
-  newGame.save()
-  .then(() => {
-    res.json(newGame.id)
-  })
-  .catch(err => res.status(400).json('Error: ' + err));
+  acs.findOne({username: game.username})
+  .then(userACS => {
+    newGame.save()
+      .then(() => {
+        res.json({instance: newGame.id, acs: userACS.acsTotal.total})
+      })
+      .catch(err => res.status(400).json('Error: ' + err));
+  }).catch(err => res.status(400).json('Error: ' + err));
+
+  
 });
 
 router.route('/next').put((req, res) => {
@@ -42,7 +47,7 @@ router.route('/next').put((req, res) => {
           userACS.acsTotal.total += game.points;
           userACS.acsTotal.picksPrediciton += game.points;
           userACS.save()
-            .then(() => res.json('Points updated'))
+            .then(() => res.json({acs: userACS.acsTotal.total, points: game.points}))
             .catch(err => res.status(400).json('Error: ' + err));
 
       }).catch(err => res.status(401).json('Error: ' + err));
@@ -50,7 +55,7 @@ router.route('/next').put((req, res) => {
     } else {
       console.log("hello");
 
-      const sendRandom = () => {
+      const sendRandom = (previous) => {
         trivia.aggregate([
           { $match: { _id: { $nin: game.questionIds } } },
           { $sample: { size: 1 } } 
@@ -59,7 +64,7 @@ router.route('/next').put((req, res) => {
           console.log("question", question);
             game.questionIds.push(question);
             game.save()
-              .then(() => res.json({ currentQuestion:question.question, options:question.options }))
+              .then(() => res.json({ currentQuestion:question.question, options:question.options, previous: previous, questionCount: game.questionIds.length }))
               .catch(err => res.status(403).json('Error: ' + err));
           }).catch(err => res.status(404).json('Error: ' + err));
       }
@@ -73,11 +78,15 @@ router.route('/next').put((req, res) => {
             if(response.answer === req.body.answer){
               console.log("step 3");
               game.points += 1;
-              game.save().then(sendRandom);
-            }              
+              game.save().then(sendRandom("correct"));
+            } else {
+              game.points -= 1;
+              game.save().then(sendRandom("wrong"));
+            }
+
           }).catch(err => res.status(402).json('Error: ' + err));
       } else {
-        sendRandom();
+        sendRandom("none");
       }
     }
 
