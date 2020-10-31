88c76ab0c0e1c33bf5d44239ece868f9668c3a58
Cherie <c.kong@mail.utoronto.ca>
2020-10-30 22:13:11 -0400

    [37] converted class to function

--- src/components/TheZone.js
+++ src/components/TheZone.js
@@ -1,40 +1,25 @@
 import React, {Component} from 'react';
-import axios from 'axios';
 import '../styling/TheZone.css';
 import PostPopup from './ProfilePostPopup';
 import Header from './Header';
 import { AuthContext } from '../Context/AuthContext';
 
-export default class TheZone extends Component {
-    //example of using authContext in class
-    //define contextType in the class using AuthContext
-    //so that we are able to access it through this.context
-    static contextType = AuthContext;
-
-    constructor(props) {
-        super(props);
-
-    }
-
+export default function TheZone() {
     //Listens for an event and creates content for a post
-    onCreatePost(e) {
+    const onCreatePost = (e) => {
         
         document.getElementById('post').style.borderColor = "transparent";
     }
 
     //Listens for an event and sets the username state
-    onComment(e) {
+    const onComment =  (e) => {
         document.getElementById('comment').style.borderColor = "transparent";
     }
+    const onLogin = () => {
 
-    //onReportPost(e){}
-
-    //onDeletePost(e) {}
-
-
-    render() {
-        return (
-            <div className="page">
+    }
+    return (
+        <div className="page">
                 <Header />
                 <div className="login">
                    
@@ -47,11 +32,10 @@ export default class TheZone extends Component {
                              />
                     </div>
                   
-                    <button className="createPostBtn" onClick={this.onLogin}>Create Post</button>
+                    <button className="createPostBtn" onClick={onLogin()}>Create Post</button>
                     <br></br>
                    
                 </div>
             </div>
-        );
-    }
+    )
 }
