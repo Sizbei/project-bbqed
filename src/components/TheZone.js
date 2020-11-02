0fb776663a7c1365ed7fa14ca17ecfcc46388082
Raymond Chen <raymondchen2009@live.cn>
2020-11-02 18:42:38 -0500

    Changed create post to different design

--- src/components/TheZone.js
+++ src/components/TheZone.js
@@ -5,35 +5,17 @@ import Header from './Header';
 import { AuthContext } from '../Context/AuthContext';
 
 export default function TheZone() {
-    //Listens for an event and creates content for a post
-    const onCreatePost = (e) => {
-        
-        document.getElementById('post').style.borderColor = "transparent";
-    }
+    
+    
 
-    //Listens for an event and sets the username state
-    const onComment =  (e) => {
-        document.getElementById('comment').style.borderColor = "transparent";
-    }
-    const onLogin = () => {
-
-    }
     return (
         <div className="page">
                 <Header />
                 <div className="login">
                    
-                    <div className="post">
-                        <label className="text">Create Post</label>
-                        <input className="input"
-                            id="post"
-                            type="string"
-                            
-                             />
-                    </div>
+                <input type="text" class="zgT5MfUrDMC54cpiCpZFu" placeholder="Create Post"></input>
                   
-                    <button className="createPostBtn" onClick={onLogin()}>Create Post</button>
-                    <br></br>
+               
                    
                 </div>
             </div>
