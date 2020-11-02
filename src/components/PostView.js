cc68e47ae7ca1b857cf02a5231553cc63be0a47c
Cherie <c.kong@mail.utoronto.ca>
2020-11-02 12:26:34 -0500

    [76] added skeleton of post view

--- src/components/PostView.js
+++ src/components/PostView.js
@@ -2,34 +2,77 @@ import React, {useState,  useEffect, Component} from "react"
 import ProfilePicture from './ProfilePicture'
 import '../styling/PostView.css'
 
-function Post(props) {
-  
-  return (
-    <div className="tzpv-post-container">
-        <div className="tzpv-user-info">
-            <ProfilePicture username ={'user3'}/>
-            <label> user3 (0) </label>
 
-        </div>
-        <div className="tzpv-post-info"> 
-        <p> </p>
-        </div>
-        
-    </div>
-  )
-}
 export default function View() {
-  
+    const [username, setUsername] = useState('user3'); 
+    const [acs, setAcs] = useState(0); 
+    const [content, setContent] = useState('testing123'); 
+    const [agree, setAgree] = useState(false); 
+    const [disagree, setDisagree] = useState(false); 
+    const [comments, setComments] = useState([
+        {username: 'user4', comment: 'blah blah', acs: 23}, 
+        {username: 'user2', comment: 'blah blah1', acs: 22}, 
+        {username: 'user1', comment: 'blah blah2', acs: 3}, 
+    ]); 
     useEffect(() => {
     
     console.log('mount it!');
   }, []);
-  
+
+    const handlePostAgree = () => {
+        setAgree(!agree);
+        console.log(!agree);
+        console.log("now: " + agree)
+    }
+    const handlePostDisagree = () => { 
+
+    }
+
+    const handleCommentAgree = () => {
+
+    }
+    const handleCommentDisagree = () => { 
+
+    }
   return (
   <div className="tzpv-background">
       <div className="tzpv-container">
-        <Post/>
+        <div className="tzpv-post-container">
+            <div className="tzpv-user-info">
+                <ProfilePicture username = {username}/>
+                <label> {username} ({acs})  </label>
 
+            </div>
+            <div className="tzpv-post-info"> 
+            <p> {content} </p>
+            </div>
+        </div>
+        <div className="tzpv-post-buttons"> 
+            <button onClick={handlePostAgree} className={agree ? "tzpv-post-button-agree-selected" :"tzpv-post-button-agree" }>   Agree  </button>
+            <button onClick={handlePostDisagree} className={disagree? "tzpv-post-button-disagree-selected" : "tzpv-post-button-disagree" }> Disagree </button>
+        </div>
+        <div className="tzpv-post-comment-container">
+            <input type="text"/>
+            <button> Post Comment </button> 
+        </div>
+        <div className="tzpv-comments-container"> 
+            {comments.map(data => {
+                return (
+                    <div className="tzpv-comment-container">
+                        <div className="tzpv-profile">
+                            <label> {data.username} ({data.acs})</label>
+                            <ProfilePicture scale={0.8} username={data.username}/>
+                            <div>
+                                <a> Agree </a>
+                                <a> Disagree </a>
+                            </div>      
+                        </div>
+                        {data.comment}
+
+                    </div>
+                )
+            })}
+        </div>
       </div>
   </div>
   )
