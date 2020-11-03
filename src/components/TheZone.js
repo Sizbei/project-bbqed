a7a7fa584181a81263041474bff15e0b0d16ebc3
Raymond Chen <raymondchen2009@live.cn>
2020-11-03 18:26:50 -0500

    Added display a single post functionatliy

--- src/components/TheZone.js
+++ src/components/TheZone.js
@@ -2,12 +2,15 @@ import React, { useEffect, useState, useContext } from 'react';
 import '../styling/TheZone.css';
 import PostPopup from './ProfilePostPopup';
 import { AuthContext } from '../Context/AuthContext';
+import ProfilePicture from './ProfilePicture'
+
 
 
 export default function TheZone(props) {
     const authContext = useContext(AuthContext);
     const postId = props.location.pathname.slice(17, props.location.pathname.length);
     const path = ' /zone/display/' + authContext.user.username + '/' + postId;
+
     const [username, setUsername] = useState('');
     const [likes, setLikes] = useState(0);
     const [acs, setAcs] = useState(0);
@@ -55,12 +58,9 @@ export default function TheZone(props) {
             .catch((error) => {
                 console.log(error);
             })
-
-
     }
 
     const handlePostDisagree = () => {
-
         const body = {
             post: postId,
             username: authContext.user.username,
@@ -86,15 +86,36 @@ export default function TheZone(props) {
     }
 
     
+    const togglePostPopup = () => {
 
+    }
 
     return (
         <div class="tzone-page">
+            
             <div class= "tzone-all-posts"> 
                 <div class="tzone-post-body">   
-                    <input type="text" class="tzone-creat-post" placeholder="Create Post"></input>
+                    <input type="text" class="tzone-create-post" onClick={togglePostPopup} placeholder="Create Post"></input>
 
                 </div>
+
+                <div className="tzone-post-container">
+                    <div className="tzone-user-info">
+                        <ProfilePicture username={username} />
+                        <label> {username} ({acs})  </label>
+                        <div className="tzone-likes"> <label> {likes} </label></div>
+                    </div>
+                    <div className="tzone-post-info">
+                        <p> {content} </p>
+
+                    </div>
+                </div>
+                <div className="tzone-post-buttons">
+                    <button onClick={handlePostAgree} className={agree ? "tzone-post-button-agree-selected" : "tzone-post-button-agree"}>   Agree  </button>
+
+                    <button onClick={handlePostDisagree} className={disagree ? "tzone-post-button-disagree-selected" : "tzone-post-button-disagree"}> Disagree </button>
+                </div>
+
             </div>
 
             
