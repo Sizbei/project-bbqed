f24cfbf4f9d395b282e340e66ad63d28598742a9
Raymond Chen <raymondchen2009@live.cn>
2020-11-02 20:01:43 -0500

    Added onclick from profile to the zone

--- src/components/TheZone.js
+++ src/components/TheZone.js
@@ -1,23 +1,34 @@
-import React, {Component} from 'react';
+import React, { useEffect, useState, useContext } from 'react';
 import '../styling/TheZone.css';
 import PostPopup from './ProfilePostPopup';
 import Header from './Header';
 import { AuthContext } from '../Context/AuthContext';
+import AuthService from '../Services/AuthService';
 
 export default function TheZone() {
 
-    
+    const authContext = useContext(AuthContext);
 
     return (
         <div className="page">
         <Header />
             <div class= "all_posts"> 
                 <div class="post_body">   
-                  <input type="text" class="post" placeholder="Create Post"></input>
-                   
+                    <input type="text" class="post" placeholder="Create Post"></input>
+
                 </div>
 
             </div>
-            </div>
+        </div>
     )
+}
+
+function togglePostPopup() {
+    this.setState({
+        showPostPopup: !this.state.showPostPopup
+    });
+}
+
+function handleRadarList() {
+    alert('Will send to pop up of all friends ??');
 }
