308f636fccb4f2f4eb59d9b583b91e5f2e893514
Cherie <c.kong@mail.utoronto.ca>
2020-11-12 22:21:11 -0500

    [48] added functions connected to backend for deleting reports

--- src/components/Reports.js
+++ src/components/Reports.js
@@ -1,19 +1,83 @@
 import { HashLink as Link } from 'react-router-hash-link';
-import React from 'react';
+import React , {useContext} from 'react';
 import "../styling/Reports.css";
+import {AuthContext} from '../Context/AuthContext';
 
 export default function Reports(props){ 
   const reports = props.reports;
   const type = props.type; 
-
-  const deleteReport = (reportId) => { 
-
+  const authContext = useContext(AuthContext);
+  const deleteReportPost = (postId) => { 
+    const body = {
+      _id: postId, 
+    }
+    fetch('/display/clearPost', {
+      method :  "post",
+      body : JSON.stringify(body),
+      headers: {
+          'Content-Type' : 'application/json'
+      }
+    }).then(res => res.json())
+    .then(data => {
+      window.location.reload(); 
+    }) 
+    .catch((error) => {
+      console.log(error);
+    })
+  }
+  const deleteReportComment = (commentId) => {
+    const body = {
+      _id: commentId, 
+    }
+    fetch('/display/clearComment', {
+      method :  "post",
+      body : JSON.stringify(body),
+      headers: {
+          'Content-Type' : 'application/json'
+      }
+    }).then(res => res.json())
+    .then(data => {
+      window.location.reload(); 
+    }) 
+    .catch((error) => {
+      console.log(error);
+    })
   }
   const deletePost = (postId) => {
-
+    const body = {
+      _id: postId, 
+    }
+    fetch('/display/' + authContext.user.username +'/deletePost', {
+      method :  "delete",
+      body : JSON.stringify(body),
+      headers: {
+          'Content-Type' : 'application/json'
+      }
+    }).then(res => res.json())
+    .then(data => {
+      deleteReportPost(postId);
+    }) 
+    .catch((error) => {
+      console.log(error);
+    })
   }
   const deleteComment = (commentId) => {
-
+    const body = {
+      _id: commentId, 
+    }
+    fetch('/display/' + authContext.user.username +'/deleteComment', {
+      method :  "delete",
+      body : JSON.stringify(body),
+      headers: {
+          'Content-Type' : 'application/json'
+      }
+    }).then(res => res.json())
+    .then(data => {
+      deleteReportComment(commentId);
+    }) 
+    .catch((error) => {
+      console.log(error);
+    })
   }
   return (
     <div>
@@ -24,7 +88,7 @@ export default function Reports(props){
             <h3> This {type} has been reported </h3> 
             <label> {data.totalReports} </label>
             <label>id: <Link to={type === "post" ? "/theZone/display/" + data._id : "/theZone/display/" + data.post + "#" + data._id}>{data._id}</Link> </label>
-            <button onClick={()=> {type === "post" ? deleteReport(data._id): deleteReport(data.post)}}> Close Report </button>
+            <button onClick={()=> {type === "post" ? deleteReportPost(data._id): deleteReportComment(data._id)}}> Close Report </button>
             <button onClick={()=> {type === "post" ? deletePost(data._id) : deleteComment(data._id)}}> Delete {type} </button>
           </div>
         )
