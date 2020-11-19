4a37a698017600a5d8fc245c56495bdcec992d16
Deon Li <34903484+lideon@users.noreply.github.com>
2020-11-19 10:12:08 -0500

    [2] Finished Issue-2 (#44)
    * [48] created report page
    
    * [86] Updated post model and zone route to handle reported posts view
    
    * [86] Updated comment model and zone route to handle reported comments view
    
    * [48] created bare bone layout for reports
    
    * [86] Fixed Zone.js routes to properly send data about reported posts and comments
    
    * [86] Fixed clearing reported posts and comments in Zone.js
    
    * [46] created seperate report lists for post and comments
    
    * [46] created two views, one for post and another for comment
    
    * [48] creating reports class for reusabilitiy
    
    * [48] editing css for reports page
    
    * [48] added functions connected to backend for deleting reports
    
    * [48] adding css and added pagination functionality
    
    * [86] Fixed bug where reported commnents route sent empty list
    
    * [48] changing  pagination styling
    
    * [48] adding styling to pagination and page
    
    * [48] Fixed fucntionalities for delete report/post/comment
    
    * [48] removed extra console line
    
    * [48] created routes so that only admin can access reports
    
    * [48] changed follow/unfollow to track/untrack
    
    Co-authored-by: Cherie <c.kong@mail.utoronto.ca>
    Co-authored-by: Cherie <45515496+Geveni@users.noreply.github.com>

--- src/components/PostView.js
+++ src/components/PostView.js
@@ -25,6 +25,7 @@ export default function View(props) {
       
         fetch(path).then(res => res.json())
         .then(data => {
+          console.log(data); 
           setUsername(data.posts.poster.username); 
           setAcs(data.posts.poster.acs); 
           setLikes(data.posts.likes); 
@@ -39,7 +40,7 @@ export default function View(props) {
         
       }, [])
     
-    const handlePostAgree = () => {
+    const handlePostAgree = async () => {
         
         const body = {
           post: postId, 
@@ -47,7 +48,7 @@ export default function View(props) {
           upvoted: agree, 
           downvoted: disagree, 
         }
-        fetch('/zone/upvote', {
+        await fetch('/zone/upvote', {
           method :  "put",
           body : JSON.stringify(body),
           headers: {
@@ -66,7 +67,7 @@ export default function View(props) {
         
         
     }
-    const handlePostDisagree = () => {
+    const handlePostDisagree = async () => {
       
       const body = {
         post: postId, 
@@ -74,7 +75,7 @@ export default function View(props) {
         upvoted: agree, 
         downvoted: disagree, 
       }
-      fetch('/zone/downvote', {
+      await fetch('/zone/downvote', {
         method :  "put",
         body : JSON.stringify(body),
         headers: {
@@ -92,7 +93,7 @@ export default function View(props) {
       })  
     }
 
-    const handleCommentAgree = (data, index) => {
+    const handleCommentAgree = async (data, index) => {
       
       const body = {
         comment: data._id, 
@@ -100,7 +101,7 @@ export default function View(props) {
         upvoted: data.upvoted, 
         downvoted: data.downvoted, 
       }
-      fetch('/zone/upvote', {
+      await fetch('/zone/upvote', {
         method :  "put",
         body : JSON.stringify(body),
         headers: {
@@ -133,14 +134,14 @@ export default function View(props) {
       })  
       
     }
-    const handleCommentDisagree = (data, index) => { 
+    const handleCommentDisagree = async (data, index) => { 
       const body = {
         comment: data._id, 
         username: authContext.user.username, 
         upvoted: data.upvoted, 
         downvoted: data.downvoted, 
       }
-      fetch('/zone/downvote', {
+      await fetch('/zone/downvote', {
         method :  "put",
         body : JSON.stringify(body),
         headers: {
@@ -175,13 +176,13 @@ export default function View(props) {
     const handleChangeCommentBody = (e) => {
       setCommentBody(e.target.value); 
     }
-    const handleAddComment = () => { 
+    const handleAddComment = async () => { 
       const body = {
         post: postId, 
         commenter: authContext.user.username, 
         body: commentBody,
       }
-      fetch('/zone/addComment', {
+      await fetch('/zone/addComment', {
         method :  "post",
         body : JSON.stringify(body),
         headers: {
@@ -225,8 +226,9 @@ export default function View(props) {
         <div className="tzpv-comments-container"> 
           <h2> Comments ({comments.length})</h2>
             {comments.map((data,index) => {
+                console.log(data._id); 
                 return (
-                    <div className="tzpv-comment-container">
+                    <div id={data._id} className="tzpv-comment-container">
                         <label className="tzpv-comment-like">{data.likes}</label>
                         <div className="tzpv-profile">
                       <label> <Link to={'/profile/' + data.commenter.username} className="tzpv-profile-link">
