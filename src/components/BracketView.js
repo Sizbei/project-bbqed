1192017745b67e0b98fe1bcaf5aecb17bdd01eba
1DWalker <alex.j.sheng.yang@gmail.com>
2020-11-21 01:24:56 -0500

    [116] Added T lines

--- src/components/BracketView.js
+++ src/components/BracketView.js
@@ -5,20 +5,64 @@ import '../styling/BracketView.css';
 
 export default function BracketView(props) {
   const canvasRef = useRef(null);
+
+  // constants
   const color = '#FFFFFF';
   const boxDim = {
     width: 200,
     height: 60,
   }
-  const minHeightDiff = 15;
+  const minHeightDiff = 10;
   const lineWidth = 1;
   const pairHeight = 2 * boxDim.height + lineWidth;
+
+  // constants regarding connecting lines
+  const minTurn = 20;
+  const minLineGap = 10;
+
+  // canvas width/height
+  const minWidth = 6 * boxDim.width;
   const minHeight = 4 * minHeightDiff + 4 * pairHeight;
-  const canvasWidth = props.width;
+  const canvasWidth = Math.max(minWidth, props.width);
   const canvasHeight = Math.max(minHeight, props.height);
   
   
+
   const [objects, setObjects] = useState([]);
+
+  const round = (n) => {
+    const m = Math.round(n);
+    // if (m % 1 === 0) {
+    //   return m + 0.5;
+    // } else {
+    //   return m;
+    // }
+    if (m < n) {
+      return m + 0.5;
+    } else {
+      return m - 0.5;
+    }
+  }
+
+  // ctx.lineTo and ctx.stroke() will draw a line where the current point is right on the center of the line. 
+  // It's easier when we work with the corners of lines, so these functions will be an adapter
+  const moveTo = (desx, desy) => {
+    const canvas = canvasRef.current;
+    const ctx = canvas.getContext('2d');
+    ctx.moveTo(round(desx), round(desy + lineWidth / 2));
+  }
+
+  const drawLineTo = (desx, desy) => {
+    const canvas = canvasRef.current;
+    const ctx = canvas.getContext('2d');
+    ctx.lineTo(round(desx), round(desy + lineWidth / 2));
+    ctx.stroke();
+  }
+
+  const drawLine = (srcx, desx, srcy, desy) => {
+    moveTo(round(srcx), round(srcy));
+    drawLineTo(round(desx), round(desy));
+  }
 
   const drawPair = (marginLeft, marginTop) => {
     const newObjects = []
@@ -27,34 +71,65 @@ export default function BracketView(props) {
     return newObjects;
   }
 
+  /* Draw the connecting lines for this:
+        a--
+          |
+          -----c
+          |
+        b--
+
+        minimum distance before the line turns is defined by the variable minTurn
+    */
+  const drawT = (a, b, c) => {
+    // Determine the direction
+    if (a.x < c.x) {
+      drawLine(a.x, a.x + minTurn, a.y, a.y);
+      drawLine(b.x, b.x + minTurn, b.y, b.y);
+      drawLine(a.x + minTurn - lineWidth / 2, a.x + minTurn - lineWidth / 2, a.y, b.y);
+      drawLine(a.x + minTurn - lineWidth / 2, a.x + minTurn - lineWidth / 2, a.y, c.y);
+      drawLine(a.x + minTurn - lineWidth, c.x, Math.round(c.y), Math.round(c.y));
+    } else {
+      drawLine(a.x, a.x - minTurn, a.y, a.y);
+      drawLine(b.x, b.x - minTurn, b.y, b.y);
+      drawLine(a.x - minTurn + lineWidth / 2, a.x - minTurn + lineWidth / 2, a.y, b.y);
+      drawLine(a.x - minTurn + lineWidth / 2, a.x - minTurn + lineWidth / 2, a.y, c.y);
+      drawLine(a.x - minTurn + lineWidth, c.x, c.y, c.y);
+    }
+  }
+
   const draw = () => {
     const canvas = canvasRef.current;
     const width = canvas.offsetWidth;
     const height = canvas.offsetHeight;
     const ctx = canvas.getContext('2d');
-
-    const newObjects = [];
-    const heightDiff = Math.max((height - 4 * pairHeight) / 3, minHeightDiff);
-
-    // Western Conference Round 1
-    Array(4).fill(0).forEach((el, i) => newObjects.push(drawPair(0, i * (pairHeight + heightDiff))))
-
-    // Eastern Conference Round 1
-    Array(4).fill(0).forEach((el, i) => newObjects.push(drawPair(width - boxDim.width, i * (pairHeight + heightDiff))))
-
-    setObjects(newObjects);
-
-    console.log(width, height);
     ctx.strokeStyle = color;
     ctx.lineWidth = lineWidth;
-    ctx.moveTo(0, boxDim.height + lineWidth / 2)
-    ctx.lineTo(boxDim.width, boxDim.height + lineWidth / 2);
-    ctx.stroke();
-    ctx.lineTo(width - boxDim.width, height - boxDim.height);
-    ctx.stroke();
-    ctx.lineTo(width, height - boxDim.height);
-    ctx.stroke();
-  }
+
+    const newObjects = [];
+    const heightGap = Math.max((height - 4 * pairHeight) / 3, minHeightDiff);
+    const heightBlock = pairHeight + heightGap;
+
+    const widthGap = (width - 6 * boxDim.width) / 5;
+    const widthBlock = boxDim.width + widthGap;
+
+    // Western Conference Round 1
+    Array(4).fill(0).forEach((el, i) => newObjects.push(drawPair(0, i * heightBlock)))
+
+    // Western Conference Semi Finals
+    const sf1Height = (0 + heightBlock + pairHeight) / 2;
+    newObjects.push(drawPair(widthBlock, sf1Height - boxDim.height));
+    drawT({x: boxDim.width + minLineGap, y: boxDim.height}, {x: boxDim.width + minLineGap, y: heightBlock + boxDim.height}, {x: 2 * widthBlock - minLineGap, y: sf1Height})
+
+
+    // Eastern Conference Round 1
+    Array(4).fill(0).forEach((el, i) => newObjects.push(drawPair(width - boxDim.width, i * heightBlock)))
+
+    setObjects(newObjects);
+    
+    // Draw connecting lines
+    drawT({x: boxDim.width + minLineGap, y: boxDim.height}, {x: boxDim.width + minLineGap, y: 2 * heightBlock + boxDim.height}, {x: width - boxDim.width - minLineGap, y: heightBlock + boxDim.height})
+    drawT({x: width - boxDim.width - minLineGap, y: boxDim.height}, {x: width - boxDim.width - minLineGap, y: 3 * heightBlock + boxDim.height}, {x: boxDim.width + minLineGap, y: height * 3 / 4})
+  }    
 
   useEffect(() => {
     draw();
@@ -67,7 +142,6 @@ export default function BracketView(props) {
     <div className="bracketview-center">
       <div className="bracketview-div">  
         <canvas id="bracket" ref={canvasRef} width={canvasWidth} height={canvasHeight}></canvas>
-        {/* <SampleRect width={200} height={100} marginLeft={400} marginTop={300} /> */}
         {objects}
       </div> 
     </div>
@@ -82,7 +156,7 @@ function Rect(props) {
 
   console.log(width, height);
   const style = {
-    backgroundColor: "green",
+    backgroundColor: "grey",
     width: width + "px",
     height: height + "px",
     marginLeft: marginLeft + "px",
