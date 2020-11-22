19b8e2936ab0aaa2725f888607453e60f38fec31
1DWalker <alex.j.sheng.yang@gmail.com>
2020-11-22 02:24:54 -0500

    [116] Added basic playoff pickems

--- src/components/Bracket.js
+++ src/components/Bracket.js
@@ -1,100 +1,7 @@
 import React, {useEffect, useRef, useState} from 'react';
 import BracketView from './BracketView.js';
 
-// // aka leaf
-// class BaseMatch {
-//   constructor(parent, top, bottom) {
-//     this.parent = parent;
-//     this.top = top;
-//     this.bottom = bottom;
-//   }
-// }
-
-// aka non-leaf node
-// class Match {
-//   constructor(parent) {
-//     this.parent = parent;
-//   }
-
-//   copy() {
-//     return new Match(this.parent);
-//   }
-
-//   update() {
-    
-//   }
-// }
-
 export default function BracketController(props) {
-  const teams = [
-    "one",
-    "two",
-    "three",
-    "four",
-    "five",
-    "six",
-    "seven",
-    "eight",
-    "nine",
-    "ten",
-    "eleven",
-    "twelve",
-    "thirteen",
-    "fourteen",
-    "fifteen",
-    "sixteen",
-  ]
-
-  const [selected, setSelected] = useState(Array(32).fill(false));
-  const [slots, setSlots] = useState(Array(32).fill(0).map((el, i) => i));
-
-  const [predictions, setPredictions] = useState({
-    "WC-QF1": null,
-    "WC-QF2": null,
-    "WC-QF3": null,
-    "WC-QF4": null, 
-    "WC-SF1": null,
-    "WC-SF2": null,
-    "WC-F": null,
-    "F": null,
-    "EC-F": null,
-    "EC-SF1": null,
-    "EC-SF2": null,
-    "EC-QF1": null,
-    "EC-QF2": null,
-    "EC-QF3": null,
-    "EC-QF4": null,
-  });
-
-  const toMatchName = (treeIndex) => {
-    if (treeIndex === 1) {
-      return 'winner';
-    }
-    // get the upper index for single representative
-    const representative = treeIndex % 2 == 0 ? treeIndex : treeIndex - 1;
-
-    const map = {
-      1: "CHAMPION",
-      2: "F",
-      4: "WC-F",
-      6: "EC-F",
-      8: "WC-SF1",
-      10: "WC-SF2",
-      12: "EC-SF1",
-      14: "EC-SF2",
-      16: "WC-QF1",
-      18: "WC-QF2",
-      20: "WC-QF3",
-      22: "WC-QF4",
-      24: "EC-QF1",
-      26: "EC-QF2",
-      28: "EC-QF3",
-      30: "EC-QF4",
-    }
-
-    return map[representative];
-  }
-  
   const codes = [
     "WC-QF1A",
     "WC-QF1B",
@@ -127,6 +34,79 @@ export default function BracketController(props) {
     "EC-QF4A",
     "EC-QF4B",
   ]
+
+  // contain the teams that are representeed in the bracket
+  const [teams, setTeams] = useState({
+    "WC-QF1A": "WC-QF1A",
+    "WC-QF1B": "WC-QF1B",
+    "WC-QF2A": "WC-QF2A",
+    "WC-QF2B": "WC-QF2B",
+    "WC-QF3A": "WC-QF3A",
+    "WC-QF3B": "WC-QF3B",
+    "WC-QF4A": "WC-QF4A",
+    "WC-QF4B": "WC-QF4B",
+    "EC-QF1A": "EC-QF1A",
+    "EC-QF1B": "EC-QF1B",
+    "EC-QF2A": "EC-QF2A",
+    "EC-QF2B": "EC-QF2B",
+    "EC-QF3A": "EC-QF3A",
+    "EC-QF3B": "EC-QF3B",
+    "EC-QF4A": "EC-QF4A",
+    "EC-QF4B": "EC-QF4B",
+  })
+
+  const [predictions, setPredictions] = useState({
+    "WC-QF1": null,
+    "WC-QF2": null,
+    "WC-QF3": null,
+    "WC-QF4": null, 
+    "WC-SF1": null,
+    "WC-SF2": null,
+    "WC-F": null,
+    "F": null,
+    "EC-F": null,
+    "EC-SF1": null,
+    "EC-SF2": null,
+    "EC-QF1": null,
+    "EC-QF2": null,
+    "EC-QF3": null,
+    "EC-QF4": null,
+  });
+
+  const [counter, setCounter] = useState(0); // ensure updates for child useEffect so that onClick remains fresh
+
+  const getOpposingTreeIndex = (treeIndex) => {
+    return treeIndex % 2 == 0 ? treeIndex + 1 : treeIndex - 1;
+  }
+
+  const toMatchName = (treeIndex) => {
+    if (treeIndex === 1) {
+      return 'winner';
+    }
+    // get the upper index for single representative
+    const representative = treeIndex % 2 == 0 ? treeIndex : treeIndex - 1;
+
+    const map = {
+      1: "CHAMPION",
+      2: "F",
+      4: "WC-F",
+      6: "EC-F",
+      8: "WC-SF1",
+      10: "WC-SF2",
+      12: "EC-SF1",
+      14: "EC-SF2",
+      16: "WC-QF1",
+      18: "WC-QF2",
+      20: "WC-QF3",
+      22: "WC-QF4",
+      24: "EC-QF1",
+      26: "EC-QF2",
+      28: "EC-QF3",
+      30: "EC-QF4",
+    }
+
+    return map[representative];
+  }
 
   // Translate 0-29 to a tree index
   // the winner is the tree index 1
@@ -172,6 +152,10 @@ export default function BracketController(props) {
     return reverse;
   }())
 
+  const hasChildren = (treeIndex) => {
+    return treeIndex < 16;
+  }
+
   const parent = (treeIndex) => {
     if (treeIndex <= 1) {
       return 'None';
@@ -184,33 +168,64 @@ export default function BracketController(props) {
     }
   }
 
+
   const onClick = (index) => {
-    const treeIndex = toTreeIndex[index];
+    setCounter(counter + 1);
 
-    // is it a new prediction?
-    // update predictions if yes
+    const teamName = codes[index];
+    let treeIndex = toTreeIndex[index];
+    
+    console.log("start", predictions);
+    const newPredictions = {...predictions};
+    const matchName = toMatchName(treeIndex);
+    const previousPrediction = predictions[matchName];
 
+    let newPrediction = null; 
 
-    // console.log(translateToTreeIndex[index])
+    if (!hasChildren(treeIndex)) {
+      newPrediction = teamName;
+      console.log("LEAF NODE");
+    } else {
+      const predictedUs = newPredictions[toMatchName(2 * treeIndex)];
+      const predictedOther = newPredictions[toMatchName(2 * getOpposingTreeIndex(treeIndex))];
 
-    // let path = toTreeIndex[index];
-    // while (path != 'None') {
-    //   console.log(toPostIndex[path]);
-    //   path = parent(path);
+      console.log("Predicted children", predictedUs, predictedOther);
+      if (predictedUs === null || predictedOther === null) {
+        console.log("Nothing to predict here.");
+      } else {
+        newPrediction = predictedUs;
+      }
+    }
+
+    newPredictions[matchName] = newPrediction;
+
+    // while (treeIndex != 1) {
+      
+
+    //   treeIndex = parent(treeIndex);
     // }
+
+    setPredictions(newPredictions);
   }
 
-  const [tree, setTree] = useState(null);
+  console.log("predictions", predictions);
 
-  useEffect(() => {
-    // const parent1 = new Match(null);
-    // const child1 = new BaseMatch(tree, "team a", "team b");
-    // const child2 = new BaseMatch(tree, "team c", "team d");
-    // setTree(parent1);
-  }, [])
+  const slots = (function(){
+    const slots = Array(30).fill(0).map((el, i) => {
+      const treeIndex = toTreeIndex[i];
+      
+      if (!hasChildren(treeIndex)) {
+        return teams[codes[i]]
+      } else {
+        const childPrediction = predictions[toMatchName(2 * treeIndex)];
+        return childPrediction;
+      }
 
-  console.log(tree);
+      return i;
+    })
+    return slots;
+  }());
 
-  return <BracketView slots={slots} onClick={onClick}/>
+  return <BracketView slots={slots} onClick={onClick} counter={counter}/>
 }
 
