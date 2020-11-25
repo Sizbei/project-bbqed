6da1dccebda76c3a574a0982f576d9efaad59f90
axelan <axelan@users.noreply.github.com>
2020-11-24 22:36:18 -0500

    [121] Initial commit for game model

---
+++ backend/models/game.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+const gameSchema = new Schema({
+    team1: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'team',
+    },
+    team2: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'team',
+    },
+    result: {
+      type: String,
+    },
+    gameDay: {
+      type: Date,
+    },
+  }, {
+    timestamps: true,
+  });
+const game = mongoose.model('game', gameSchema);
+module.exports = game;
