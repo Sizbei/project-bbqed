dba1868cee2f40b53c8a2e274cfe2f2a4c89739a
Hongfei Chen <hongfei.chen@mail.utoronto.ca>
2020-11-26 20:21:26 -0500

    [119] add route for submit a prediction

--- backend/routes/prediction.js
+++ backend/routes/prediction.js
@@ -8,9 +8,76 @@ const passport = require('passport');
 const jwt = require('jsonwebtoken');
 const passportConfig = require('../passport');
 
-router.route('/addSeasonGames').put((req, res) => {
+const demo_date = new Date(2020, 7, 29);
 
+const findUserPick = (picks, user) => {
+    for(let index in picks) {
+        if(picks[index].user == user) {
+            return picks[index];
+        }
+    }
+    return null;
+}
 
+//req body: {_id: str, user: str, pick: str}
+router.route('/addPrediction').put(async (req, res) => {
+//router.route('/addPrediction').put(passport.authenticate('jwt', {session : false}), async (req, res) => {
+    const user = req.body.user;
+    //const user = req.user.username;
+    const cur_date = demo_date; //since games in DB are all historical data, use a demo date to represent current date
+    prediction.findOne({game: req.body._id}).then(prediction => {
+        let cur_pick = null;
+        if(!prediction || !req.body.pick) {
+            res.status(400).json({err: "Bad request: invaild information in request"});
+        } else if(prediction.closeTime < cur_date) {
+            res.status(400).json({err: "Bad request: prediction for this game is closed"});
+        } else if(cur_pick = findUserPick(prediction.picks, user)) {
+            cur_pick.pick = req.body.pick;
+            prediction.save();
+            res.json({msg: "pick updated"})
+        } else {
+            cur_pick = {
+                user: user,
+                pick: req.body.pick
+            }
+            prediction.picks.push(cur_pick);
+            prediction.save();
+            res.json({msg: "pick submitted"})
+        }
+    }).catch(err => res.status(500).json({err: err}));
+})
+
+//req body: {games: [{team1: str, team2: str, result: str, year: int, month: int, day: int}]}
+router.route('/addGames').put(async (req, res) => {
+    for(let index in req.body.games){
+        const cur_game = req.body.games[index];
+        const team1 = await team.findOne({name: cur_game.team1}).then(team => {return team});
+        const team2 = await team.findOne({name: cur_game.team2}).then(team => {return team});
+        if(!team1) {
+            res.status(400).json({err: cur_game.team1 + " does not exist"});
+        }
+        if(!team2) {
+            res.status(400).json({err: cur_game.team2 + " does not exist"});
+        }
+        const g = new game({
+            team1: team1,
+            team2: team2,
+            result: cur_game.result,
+            type: "seasonal",
+            gameDay: new Date(cur_game.year, cur_game.month, cur_game.day)
+        })
+        const p = new prediction({
+            game: g,
+            closeTime: g.gameDay,
+            status: "open",
+            acsStatus: "pend",
+            picks: []
+        })
+        g.prediction = p;
+        g.save();
+        p.save();
+    }
+    res.json("done");
 })
 
 module.exports = router;
