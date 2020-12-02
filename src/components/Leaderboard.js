d7756d39f9fa0956e811d2f99e421bc0dfd17f19
Andy Phy Lim <Andyphyl00@gmail.com>
2020-12-02 02:57:15 -0500

    [120] Add buttons to leaderboard

--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -2,64 +2,133 @@ import React, {useState,  useEffect} from "react"
 import '../styling/Leaderboard.css'
 
 export default function Leaderboard(props) {
-    const [users, setUsers] = useState([]);
+    const[globalBGStyle, setGlobalBGStyle] = useState();
+    const[radarBGStyle, setRadarBGStyle] = useState();
+    const[globalVisStyle, setGlobalVisStyle] = useState();
+    const[radarVisStyle, setRadarVisStyle] = useState();
+    const[regSeasonBGStyle, setRegSeasonBGStyle] = useState();
+    const[playOffBGStyle, setPlayOffBGStyle] = useState();
+
     var mode = props.mode;
-     const givenUsers = props.users;
+    const givenUsers = props.users;
+    var leaderboardRadarStyle;
+    var leaderboardGlobalStyle;
+    var globalStyle;
+    var radarStyle;
+    var regSeasonStyle;
+    var playOffStyle;
 
     useEffect(() => {
         //fetch and setUsers
+        globalStyle = { "background-color": "#0B0A0A" };
+        radarStyle = { "background-color": "#c8651b" };
+        leaderboardRadarStyle = { "visibility": "visible"};
+        leaderboardGlobalStyle = { "visibility": "hidden"};
+        setGlobalBGStyle(globalStyle);
+        setRadarBGStyle(radarStyle);
+        setGlobalVisStyle(leaderboardGlobalStyle);
+        setRadarVisStyle(leaderboardRadarStyle);
+
+        regSeasonStyle = { "background-color": "#c8651b" };
+        playOffStyle = {"background-color": "#0B0A0A" };
+        setRegSeasonBGStyle(regSeasonStyle);
+        setPlayOffBGStyle(playOffStyle);
     }, [])
     
 // /prediction/leaderboard
 // /prediction/regularseason/global
 // /prediction/playoff
+    const changeTab = (type) => {
+        if (type === "radar") {
+            leaderboardRadarStyle = { "visibility": "hidden"};
+            leaderboardGlobalStyle = { "visibility": "visible"};
+            setGlobalVisStyle(leaderboardGlobalStyle);
+            setRadarVisStyle(leaderboardRadarStyle);
 
-    if (mode === "Global") {
+            globalStyle = { "background-color": "#c8651b" };
+            radarStyle = { "background-color": "#0B0A0A" };
+            setGlobalBGStyle(globalStyle);
+            setRadarBGStyle(radarStyle);   
+        }
+        else if (type === "global") {
+            globalStyle = { "background-color": "#0B0A0A" };
+            radarStyle = { "background-color": "#c8651b" };
+            leaderboardRadarStyle = { "visibility": "visible"};
+            leaderboardGlobalStyle = { "visibility": "hidden"};
+            setGlobalBGStyle(globalStyle);
+            setRadarBGStyle(radarStyle);
+            setGlobalVisStyle(leaderboardGlobalStyle);
+            setRadarVisStyle(leaderboardRadarStyle);
+        }
+        else if (type === "regSeason") {
+            regSeasonStyle = { "background-color": "#0B0A0A" };
+            playOffStyle = {"background-color": "#c8651b" };
+            setRegSeasonBGStyle(regSeasonStyle);
+            setPlayOffBGStyle(playOffStyle);
+        }
+        else if (type === "playOff") {
+            regSeasonStyle = { "background-color": "#c8651b" };
+            playOffStyle = {"background-color": "#0B0A0A" };
+            setRegSeasonBGStyle(regSeasonStyle);
+            setPlayOffBGStyle(playOffStyle);
+        }
+    }
         return (
-            <div className='all'>
-                <div className='Leaderboard'>
-                    <GlobalHeaderRow></GlobalHeaderRow>
-                    {givenUsers.map((data, index) => {
-                        console.log(data);
-                        if(index % 2 == 0) {
-                            //<GlobalRow color='#0B0A0A' rank='1' numofplayers='20' percentofplayers='20' division='x' points='2' ></GlobalRow>
+            <div>
+                <div className='leaderboardGlobal' style = {globalVisStyle}>
+                    <div className="leaderboardButtonDiv">
+                        <button className="leaderboardButton" style = {regSeasonBGStyle} onClick = {() => changeTab("playOff")}>Regular Season</button>
+                        <button className="leaderboardButton" style = {playOffBGStyle} onClick = {() => changeTab("regSeason")}>Playoff</button>
+                        <div className='centerSpace'></div>
+                        <button className="leaderboardButton" style = {globalBGStyle}>Global</button>
+                        <button className="leaderboardButton" style = {radarBGStyle}  onClick = {() => changeTab("global")}>Radar</button>
+                    </div>
+                
+                    <div className='Leaderboard'>
+                        <GlobalHeaderRow></GlobalHeaderRow>
+                        {givenUsers.map((data, index) => {
+                            console.log(data);
+                            if(index % 2 == 0) {
+                                //<GlobalRow color='#0B0A0A' rank='1' numofplayers='20' percentofplayers='20' division='x' points='2' ></GlobalRow>
+                                return (
+                                    <GlobalRow rank={index+1} color='#0B0A0A'></GlobalRow>
+                                )
+                            }
+                            //<GlobalRow rank='1' numofplayers='20' percentofplayers='20' division='x' points='2' ></GlobalRow>
                             return (
-                                <GlobalRow rank={index+1} color='#0B0A0A'></GlobalRow>
+                                <GlobalRow rank={index+1}></GlobalRow>
                             )
-                        }
-                        //<GlobalRow rank='1' numofplayers='20' percentofplayers='20' division='x' points='2' ></GlobalRow>
-                        return (
-                            <GlobalRow rank={index+1}></GlobalRow>
-                        )
-                    })}
-                </div>
-            </div> 
-        );
-    } 
-    else if (mode === "Radar") {
-        return (
-            <div className='all'>
-
-            <div className='Leaderboard'>
-                <RadarHeaderRow></RadarHeaderRow>
-                {givenUsers.map((data, index) => {
-                    console.log(data);
-                    if(index % 2 == 0) {
-                        //<RadarRow color='#0B0A0A' rank='1' name='andy' points='23'></RadarRow>
-                        return (
-                            <RadarRow rank={index+1} color='#0B0A0A'></RadarRow>
-                        )
-                    }
-                    //<RadarRow color='#0B0A0A'></RadarRow>
-                    return (
-                        <RadarRow rank={index+1}></RadarRow>
-                    )
-                })}
+                        })}
+                    </div>
+                </div> 
+                <div className='leaderboardRadar' style = {radarVisStyle}>
+                    <div className="leaderboardButtonDiv">
+                        <button className="leaderboardButton" style = {regSeasonBGStyle}  onClick = {() => changeTab("playOff")}>Regular Season</button>
+                        <button className="leaderboardButton" style = {playOffBGStyle}  onClick = {() => changeTab("regSeason")}>Playoff</button>
+                        <div className='centerSpace'></div>
+                        <button className="leaderboardButton" style = {globalBGStyle} onClick = {() => changeTab("radar")}>Global</button>
+                        <button className="leaderboardButton" style = {radarBGStyle} >Radar</button>
+                    </div>
+                    <div className='Leaderboard'>
+                        <RadarHeaderRow></RadarHeaderRow>
+                        {givenUsers.map((data, index) => {
+                            console.log(data);
+                            if(index % 2 == 0) {
+                                //<RadarRow color='#0B0A0A' rank='1' name='andy' points='23'></RadarRow>
+                                return (
+                                    <RadarRow rank={index+1} color='#0B0A0A'></RadarRow>
+                                )
+                            }
+                            //<RadarRow color='#0B0A0A'></RadarRow>
+                            return (
+                                <RadarRow rank={index+1}></RadarRow>
+                            )
+                        })}
+                    </div>
                 </div>
             </div>
         );
     }
-}
 
 function GlobalHeaderRow() {
     return (
@@ -78,6 +147,7 @@ function RadarHeaderRow(props) {
     return (
         <div className='Leaderboard-row'>
             <div className='L-col-Radar'>Rank</div>
+            <div className='L-col-Radar-Space'>.</div>
             <div className='L-col-Radar'>Name</div>
             <div className='L-col-Radar'>Points</div>
         </div>
@@ -110,6 +180,7 @@ function RadarRow(props) {
     const rank = "rank" in props ? props.rank : 0;
     const name = "name" in props ? props.name : "-";
     const points = "points" in props ? props.points : "-";
+    const profilePic = "profilePIc" in props ? props.profile : "https://cdn.onlinewebfonts.com/svg/img_258083.png"
     const backgroundColor = "color" in props ? props.color : "1b1a18"
 
     useEffect(() => {
@@ -118,8 +189,8 @@ function RadarRow(props) {
 
     return (
         <div className='Leaderboard-row' style={backgroundStyle}>
-            <div className='L-col-Image'></div>
             <label className='L-col-Radar'>{rank}</label>
+            <img className='L-col-Radar-Image' src={profilePic}></img>
             <label className='L-col-Radar'>{name}</label>
             <label className='L-col-Radar'>{points}</label>
         </div>
