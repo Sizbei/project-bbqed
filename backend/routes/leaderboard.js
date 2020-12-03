4b46f6f61a8e8dbc9b11d0e8c857e72761384c38
axelan <axelan@users.noreply.github.com>
2020-12-02 21:23:01 -0500

    [115] Initial comimt for leaderboard routes

---
+++ backend/routes/leaderboard.js
@@ -0,0 +1,152 @@
+const router = require('express').Router();
+let predictionPoints = require('../models/predictionPoints');
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
+const passportConfig = require('../passport');
+const { json } = require('express');
+
+const divisions = [
+
+  {max: 100, min: 90},
+  {max: 89, min: 80},
+  {max: 79, min: 70},
+  {max: 69, min: 60},
+  {max: 59, min: 50},
+  {max: 49, min: 40},
+  {max: 39, min: 30},
+  {max: 29, min: 20},
+  {max: 19, min: 10},
+  {max: 9, min: 0}
+
+]
+
+async function returnGlobalLeaderboard(year, category, res){
+
+  var divisionThreshhold = [0,0,0,0,0,0,0,0,0,0];
+  var divisionCounts = [0,0,0,0,0,0,0,0,0,0];
+  var divisionPercentage = [0,0,0,0,0,0,0,0,0,0];
+
+  const totalUsers = (await predictionPoints.aggregate([{$match: {year: year, category: category}}, {$project: {total: {$size: "$userPoints"}}}]))[0].total
+  
+  divisions.forEach(async function(division, index){
+    
+    predictionPoints.aggregate([{$match: {year: year, category: category}}, 
+                                {$project: {
+                                  divisionCount: {
+                                    $size: {
+                                      $filter: {
+                                        "input": "$userPoints", 
+                                        "as": "userPoints", 
+                                        "cond": {
+                                          "$and":[
+                                            {"$gte": ["$$userPoints.points", division.min]},
+                                            {"$lte": ["$$userPoints.points", division.max]}
+                                          ]
+                                        }}}}}}])
+    .then(async count => {
+
+      divisionThreshhold[index] = division.max;
+      divisionCounts[index] = count[0].divisionCount;
+      divisionPercentage[index] = count[0].divisionCount / totalUsers * 100;
+
+      if(index == divisions.length - 1){
+        res.json( {
+          divisionThreshhold: divisionThreshhold, 
+          divisionCounts: divisionCounts, 
+          divisionPercentage: divisionPercentage
+        })
+        
+      }
+
+    })
+
+  });
+
+}
+
+router.route("/regularseason/global/:year").get(async(req, res) => {
+
+  returnGlobalLeaderboard(req.params.year, "regularSeason", res);
+  
+});
+
+router.route("/regularseason/radarlist//:year/:user").get((req, res) => {
+  predictionPoints.findOne({year: req.params.year, category: "regularSeason"})
+  .then(userPoints => {
+    
+  })
+});
+
+router.route("/playoff/global/:year").get((req, res) => {
+
+  returnGlobalLeaderboard(req.params.year, "playoff", res);
+  
+});
+
+router.route("/playoff/radarlist/:year/:user").get((req, res) => {
+  predictionPoints.findOne({year: req.params.year, category: "playoff"})
+  .then(userPoints => {
+    
+  })
+});
+
+router.route("/add/board").post((req, res) => {
+
+  const year = req.body.year;
+  const category = req.body.category;
+  const userPoints = req.body.userPoints;
+
+  predictionPoints.findOne({year: year, category: category})
+  .then(found => {
+    if(!found){
+      const newPredictionPoints = new predictionPoints({
+        year: year,
+        category: category,
+        userPoints: userPoints
+      })
+
+      newPredictionPoints.save()
+        .then(() => {res.json("Document added")});
+
+    } else {
+      res.json("Document already exists")
+    }
+
+  })
+
+});
+
+router.route("/update").put((req, res) => {
+
+  const year = req.body.year;
+  const category = req.body.category;
+  const user = req.body.user;
+  const points = req.body.points;
+
+  var options = {new: true, setDefaultsOnInsert: true, useFindAndModify: false};
+
+  predictionPoints.findOneAndUpdate({year: req.body.year, category: req.body.category, userPoints: {$elemMatch: {user: user}}},
+                                    {$inc: {"userPoints.$.points": points}},
+                                    options)
+  .then(pp => {
+    if(!pp){
+
+      const newUser = {
+        user: user,
+        points: points
+      }
+
+      predictionPoints.findOne({year: req.body.year, category: req.body.category})
+      .then(pp2 => {
+        pp2.userPoints.push(newUser);
+        pp2.save().then(() => {res.json("Points updated")})
+      })
+    } else {
+      res.json("Points updated")
+    }
+  })
+
+});
+
+
+module.exports = router;
